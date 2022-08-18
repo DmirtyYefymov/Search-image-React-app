27d0029266eb3456b88ab74771920d4288a39ae8
Dmitry Yefimov <efimov.dmitry.33@gmail.com>
2022-08-18 12:42:00 +0300

    search-app-v2

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -2,7 +2,7 @@ export const fetchImages = (query = '', pageNumber = 1) => {
   return fetch(
     `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=27051173-ae4baac94b4acf795cbbda9eb&image_type=photo&orientation=horizontal&per_page=12`,
   )
-    .then(x => new Promise(resolve => setTimeout(() => resolve(x), 1000))) // Задержка добавлена для тестирования Loader
+    .then(x => new Promise(resolve => setTimeout(() => resolve(x), 1000)))
     .then(res => res.json())
     .then(data => data.hits);
 };
